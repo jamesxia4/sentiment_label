37f7cd72326b29f4501699bb239974b15ac38838
Yuanfang Xia <johnnyrxia4@126.com>
2015-09-11 16:09:54 +0800

    需求变更，修改数据库设计

---
+++ database/create_table.sql
@@ -0,0 +1,49 @@
+create database netease_gdas;
+USE netease_gdas;
+
+CREATE TABLE IF NOT EXISTS label_user
+(
+	user_id varchar(255) NOT NULL PRIMARY KEY,
+	num_current_task BIGINT(11) NOT NULL,
+	num_finished_task BIGINT(11) NOT NULL,
+	total_labeled BIGINT(11) NOT NULL,
+	INDEX(total_labeled)
+);
+
+CREATE TABLE IF NOT EXISTS label_task
+(
+	task_id BIGINT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	start_time DATETIME NOT NULL,
+	end_time DATETIME NOT NULL,
+	INDEX(end_time)
+);
+
+create table if not exists label_user_task
+(
+    task_id bigint(11) not null,
+    user_id varchar(255) not null,
+    kappa float,
+    num_effective int(11),
+    progress int(11) not null,
+    Primary key(task_id,user_id)
+);
+
+create table if not exists label_ods
+(
+	 ods_sentence_id bigint(11) not null,
+     date_id int(11) not null,
+     game_id int(11) not null,
+     source_id bigint(11) not null,
+     comment_id bigint(11) not null,
+     sentence_index int(11) not null,
+     concept_id bigint(11) not null,
+     concept_name varchar(255),
+     src_content varchar(3000),
+     content varchar(500),
+     sentiment float,
+     is_conflict int(11),
+     is_relevent int(11),
+     task_id bigint(11) not null,
+     user_id varchar(255) not null,
+     Primary key(ods_sentence_id,task_id,user_id)
+);
