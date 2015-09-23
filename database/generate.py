7a563d5741d96bfae387204313ee9f66aedd6ae9
Yuanfang Xia <johnnyrxia4@126.com>
2015-09-23 18:04:34 +0800

    和谐后的第一次提交，添加了测试集生成代码与测试集

---
+++ database/generate.py
@@ -0,0 +1,13 @@
+# -*- coding: cp936 -*-
+import random
+for i in range(1,201):
+    f=float(random.randint(-1,1))
+    print "Insert into label_ods_rst values(%d,20150923,1,1,1,1,1,'百度贴吧','画面','这个游戏画面真糟','这个游戏画面真糟，但是还是挺好玩的',%f,0,1,5,'James');"%(i,f)
+    
+for i in range(1,201):
+    f=float(random.randint(-1,1))
+    print "Insert into label_ods_rst values(%d,20150923,1,1,1,1,1,'百度贴吧','画面','这个游戏画面真糟','这个游戏画面真糟，但是还是挺好玩的',%f,0,1,5,'John');"%(i,f)
+
+for i in range(1,201):
+    f=float(random.randint(-1,1))
+    print "Insert into label_ods_rst values(%d,20150923,1,1,1,1,1,'百度贴吧','画面','这个游戏画面真糟','这个游戏画面真糟，但是还是挺好玩的',%f,0,1,5,'Mary');"%(i,f)
