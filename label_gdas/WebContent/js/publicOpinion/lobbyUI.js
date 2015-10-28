287c9533bc6dd85d2f15a110c49354e1051de40f
jamesxia4 <johnnyrxia4@126.com>
2015-10-29 01:25:08 +0800

    speical effects added

--- label_gdas/WebContent/js/publicOpinion/lobbyUI.js
+++ label_gdas/WebContent/js/publicOpinion/lobbyUI.js
@@ -3,6 +3,7 @@
  */
 
 $(document).ready(function(){
+	//任务组选择器
 	$(".label_taskgroup_item").click(function(e){
 		e.preventDefault();
 		if(!$(this).hasClass("taskgroup_item_selected")){
@@ -14,10 +15,19 @@ $(document).ready(function(){
 			});
 		}
 	});
+	
+	//任务下拉信息
 	$(".label_item_pulldown").click(function(e){
 		e.preventDefault();
 		if(!$(this).hasClass("infoPulledDown")){
+			$(this).parent().siblings(".label_lobby_tasks_item").children(".label_item_pulldown").each(function(i,e){
+				if($(e).hasClass("infoPulledDown")){
+					$(e).removeClass("infoPulledDown");
+				}
+			});
 			$(this).addClass("infoPulledDown");
+		} else {
+			$(this).removeClass("infoPulledDown");
 		}
 	});
 });
