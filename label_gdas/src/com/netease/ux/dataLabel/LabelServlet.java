ae562a3fddca63cad47cf601be5749ab72ac90c0
Yuanfang Xia <johnnyrxia4@126.com>
2015-11-04 18:27:59 +0800

    Minor Changes

--- label_gdas/src/com/netease/ux/dataLabel/LabelServlet.java
+++ label_gdas/src/com/netease/ux/dataLabel/LabelServlet.java
@@ -29,14 +29,12 @@ public class LabelServlet extends HttpServlet {
      */
     public LabelServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * 
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
@@ -44,7 +42,6 @@ public class LabelServlet extends HttpServlet {
 	 * 
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
--- label_gdas/src/com/netease/ux/dataLabel/MyTaskServlet.java
+++ label_gdas/src/com/netease/ux/dataLabel/MyTaskServlet.java
@@ -29,14 +29,12 @@ public class MyTaskServlet extends HttpServlet {
      */
     public MyTaskServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
@@ -44,7 +42,6 @@ public class MyTaskServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
