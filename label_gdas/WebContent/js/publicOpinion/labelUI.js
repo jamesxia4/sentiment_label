a95ffca3991e7545d64a48b2591f50b1b3ee7f16
Yuanfang Xia <johnnyrxia4@126.com>
2015-11-10 18:05:22 +0800

    进度条完成90%,指示器样式待UI修正

--- label_gdas/WebContent/js/publicOpinion/labelUI.js
+++ label_gdas/WebContent/js/publicOpinion/labelUI.js
@@ -11,18 +11,42 @@ function getUrlParam(name){
 
 function renderProgressBar(progress){
 		var numToPaintGreen=Math.floor(progress/10);
-		console.log(numToPaintGreen);
-		for(var i=0;i<9;i++){
+		var barWidth;
+		//根据progressBar宽度调整刻度
+		
+		//添加刻度
+		for(var i=0;i<10;i++){
 			if(i==0){
 				$("<div class=\"label_label_progressBarDotUnDone\"></div>").appendTo($(".label_label_progressBar"));
 			}
 			else{
-				$("<div class=\"label_label_progressBarDotUnDone\"></div>").appendTo($(".label_label_progressBar")).css("left",(i*11).toString()+"%");
+				barWidth=parseInt($(".label_label_progressBar").css("width").slice(0,-2));
+				console.log(barWidth);
+				$("<div class=\"label_label_progressBarDotUnDone\"></div>").appendTo($(".label_label_progressBar")).css("left",(Math.ceil(i*barWidth/10)).toString()+"px");
 			}
 		}
+		
+		//画出进度条
+		$(".label_label_progressBarDoneLayer").css("width",(progress/100*barWidth).toString()+"px");
+		if(progress>0){
+		$("<div class=\"label_label_progressBarDotIndex\"></div>").appendTo($(".label_label_progressBar")).css("left",(progress/100*barWidth-1).toString()+"px");}
+		else{
+			$("<div class=\"label_label_progressBarDotIndex\"></div>").appendTo($(".label_label_progressBar")).css("left",(progress/100*barWidth-3).toString()+"px");
+		}
+		
+		//根据计算结果调整刻度颜色
+		$(".label_label_progressBarDotUnDone").each(function(i,e){
+			if(numToPaintGreen<0){
+				return;
+			}
+			else{
+				numToPaintGreen--;
+				$(this).removeClass("label_label_progressBarDotUnDone").addClass("label_label_progressBarDotDone");
+			}
+		});
 }
 
 $(document).ready(function(){
 	console.log("start");
-	renderProgressBar(97);
+	renderProgressBar(50);
 });
