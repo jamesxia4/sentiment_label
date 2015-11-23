57cd4d08fdbe6dfbe65dfd779ed407a6f980e956
Yuanfang Xia <johnnyrxia4@126.com>
2015-11-23 18:10:56 +0800

    排名页框架初步搭建

--- label_gdas/WebContent/js/publicOpinion/rankUI.js
+++ label_gdas/WebContent/js/publicOpinion/rankUI.js
@@ -1,3 +1,70 @@
 /**
  * 
  */
+
+//测试数据，撸完就删
+var testData={}
+
+var testData1={}
+
+//用来获取页面url参数
+function getUrlParam(name)
+{
+	var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+	var r = window.location.search.substr(1).match(reg);  //匹配目标参数
+	if (r!=null) return unescape(r[2]); return null; //返回参数值
+} 
+
+//ajax调用servlet接口获取json并渲染页面
+
+/*function reloadRank(){
+	$(document).ready(function(){
+		var url_to_use="/label_gdas/rank?user_id="+getUrlParam('user_id');
+		$.ajax({
+			type:"GET",
+			url:url_to_use,
+			dataType:"json",
+			success: function(data){
+				renderRank(data);
+				addGadgets(data);
+			}
+		});
+	});
+}*/
+
+/*//渲染页面,添加html骨架
+function renderRank(JsonData){
+	$(document).ready(function(){
+		var dataLength=0;
+		for(var item in JsonData){
+			dataLength++;
+		}
+	});
+}*/
+
+//渲染页面,添加特效
+function addGadgets(){
+	$(document).ready(function(){
+		//任务组选择器
+		$(".label_taskgroup_item").click(function(e){
+			e.preventDefault();
+			if(!$(this).hasClass("taskgroup_item_selected")){
+				$(this).addClass("taskgroup_item_selected");
+				$(this).siblings(".label_taskgroup_item").each(function(i,e){
+					if($(e).hasClass("taskgroup_item_selected")){
+						$(e).removeClass("taskgroup_item_selected");
+					}
+				});
+			}
+		});
+	});
+}
+
+$(document).ready(function(){
+	addGadgets();
+});
+
+/*//Entry
+reloadRank();*/
+
+
--- label_gdas/WebContent/publicOpinion/rank.jsp
+++ label_gdas/WebContent/publicOpinion/rank.jsp
@@ -12,6 +12,7 @@
 <body>
 	<div id="label_main_content">
 		<div class="label_rank_container">
+		
 			<div class="label_rank_header">
 				<div class="label_rank_wrapper">
 					<div class="label_rank_header_logo">
@@ -21,18 +22,28 @@
 				</div>
 				<div class="label_rank_header_spliter"></div>
 			</div>
-			
-			<div class="label_myTask_task_grid">
-				<div class="label_myTask_tasks">
-					<div class="label_myTask_subHeader">未完成任务</div>
-					<!-- 以下是所有未完成任务的罗列  -->
-				</div>
 				
-				<div class="label_myTask_finishedTasks">
-					<div class="label_myTask_subHeader">已完成任务</div>
-					<!-- 以下是所有已完成任务的罗列  -->
+			<div class="label_rank_grid">
+				<div class="label_taskgroup_wrapper">
+					<div class="label_task_group_selector">
+					<!--//TODO:加上期数选择 现在先写死 20151026 --> 
+						<div class="label_taskgroup_item">2015年 第1期</div>
+						<div class="label_taskgroup_item">2015年 第2期</div>
+						<div class="label_taskgroup_item">全部</div>
+					</div>
 				</div>
 			</div>
+			
+			<div class="label_rank_header">
+				<div class="label_rank_wrapper">
+					<div class="label_rank_header_logo">
+						<img src="../image/publicOpinion/label_logo.png">
+					</div>
+					<div class="label_rank_header_text">积分奖励规则</div>
+				</div>
+				<div class="label_rank_header_spliter"></div>
+			</div>
+			
 		</div>
 	</div>
 </body>
